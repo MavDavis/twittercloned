e369e9c165d468fe5e54579a75239d5bd6f31e9a
Mavdavis <ekwesimarvelous@gmail.com>
2022-11-03 12:32:49 +0100

    commit

--- pages/Notification.vue
+++ pages/Notification.vue
@@ -3,11 +3,16 @@
     <div class="w-3/5 border-x min-h-screen h-full">
       <div class="flex w-full justify-between items-center">
         <page-header name="Notification" />
-        <div class="w-8 h-8 rounded-full hover:bg-slate-">
-          <i class="fa-regular fa-gear"></i>
+        <div class="w-fit h-fit mr-4 px-2 py-1 cursor-pointer rounded-full hover:bg-slate-200">
+          <i class="fa-solid fa-gear"></i>
 
         </div>
       </div>
+    <div class="flex px-28 border-b pb-4 justify-between items-center">
+      <p class="font-semibold cursor-pointer text-sm" :class="[allActive? '':'']">All</p> 
+      <p class="font-semibold cursor-pointer text-sm" :class="[mentionsActive? '':'']">Mentions</p>
+    </div>
+    
     </div>
     <!-- Right Sidebar -->
     <div class="w-2/5">
@@ -17,6 +22,9 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const allActive = ref(false)
+const mentionsActive = ref(false)
+</script>
 
 <style></style>
